3c788397738f5b55f899db506da5b9ad59c9bb97
ciciciciwang <xijun116@gmail.com>
2015-04-03 15:51:49 -0500

    Cucumber tests for User story 1

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
    #root 'companies#index'
 	root 'students#index'
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+     get 'stuents/:id' => 'stuents#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
---
+++ myapp/features/support/factories.rb
@@ -0,0 +1,9 @@
+# Factory.define :StudentInfo do |f|
+#   f.Name 'John'
+#   f.Uin '12354145'
+#   f.Email 'afdldw@alkdfjl.com'
+#   f.UScitizen 'true'
+#   f.Degree 'Undergraduate'
+#   f.Position_type 'Intern'
+
+# end

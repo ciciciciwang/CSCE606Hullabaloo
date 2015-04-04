25b124daf66b2d64fb6b606d61cc6387f60298ab
hongyuheng <hongyuheng@tamu.edu>
2015-04-04 15:08:47 -0500

    rspec version but some test failed

---
+++ myapp/spec/views/students/show.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "students/show", type: :view do
+  before(:each) do
+    @student = assign(:student, Student.create!(
+      :name => "Name",
+      :UIN => "Uin",
+      :email => "Email",
+      :US_citizen => false,
+      :degree => "Degree",
+      :position_type => "Position Type"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/Uin/)
+    expect(rendered).to match(/Email/)
+    expect(rendered).to match(/false/)
+    expect(rendered).to match(/Degree/)
+    expect(rendered).to match(/Position Type/)
+  end
+end

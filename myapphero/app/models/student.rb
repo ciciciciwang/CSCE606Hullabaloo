cd9ea8035dd983b1607fcb2b602fc595cbcd402f
ciciciciwang <xijun116@gmail.com>
2015-04-08 15:39:27 -0500

    user_management_with_rspec

--- myapphero/app/models/student.rb
+++ myapphero/app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ActiveRecord::Base
+	validates :name, presence: true
+	validates :UIN, presence: true
+	validates :email, presence: true
+	validates :US_citizen, presence: true
+	validates :degree, presence: true
+	validates :position_type, presence: true
 end
---
+++ myapphero/spec/views/students/edit.html.erb_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "students/edit", type: :view do
+  before(:each) do
+    @student = assign(:student, Student.create!(
+      :name => "MyString",
+      :UIN => "MyString",
+      :email => "MyString",
+      :US_citizen => false,
+      :degree => "Graduate",
+      :position_type => "Intern"
+    ))
+  end
+
+  # it "renders the edit student form" do
+  #   render
+
+  #   assert_select "form[action=?][method=?]", student_path(@student), "post" do
+
+  #     assert_select "input#student_name[name=?]", "student[name]"
+
+  #     assert_select "input#student_UIN[name=?]", "student[UIN]"
+
+  #     assert_select "input#student_email[name=?]", "student[email]"
+
+  #     assert_select "input#student_US_citizen[name=?]", "student[US_citizen]"
+
+  #     assert_select "input#student_degree[name=?]", "student[degree]"
+
+  #     assert_select "input#student_position_type[name=?]", "student[position_type]"
+  #   end
+  # end
+end

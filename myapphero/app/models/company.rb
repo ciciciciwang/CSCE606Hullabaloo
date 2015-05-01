7132277a1cc811095ff58ad3338e092aed89a54d
ciciciciwang <xijun116@gmail.com>
2015-05-01 11:18:15 -0500

    views updated

--- myapphero/app/models/company.rb
+++ myapphero/app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :name,:contact_person,:contact_email,:sponsor_level,:citizenship,:job_type,:student_level,:rep_1, presence: true
+	validates :name,:contact_person,:contact_email,:sponsor_level,:citizenship,:job_type,:student_level,:rep_1, :clinic_1_rep_no,:clinic_2_rep_no,:clinic_3_rep_no,:intvw_1_rep_no,:intvw_2_rep_no, presence: true
   	validates :contact_email, presence: true, length: { maximum: 255 },
        		          format: { with: VALID_EMAIL_REGEX },
          	          uniqueness: { case_sensitive: false }
